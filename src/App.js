c3688d84a27fa0dc42d72faf5c8c21b96c9e18ed
PopovDmytro <dimahom@ukr.net>
2018-07-23 22:23:46 +0300

    header, side nav components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+// styles
+import './scss/app.scss'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="root">
+                <h1>test</h1>
+            </div>
+        );
+    }
 }
 
 export default App;
