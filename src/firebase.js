15276f9881912dbfc1f47fb1d6687600adb344c7
PopovDmytro <dimahom@ukr.net>
2018-08-01 23:03:27 +0300

    firebase functions queries

--- src/firebase.js
+++ src/firebase.js
@@ -13,8 +13,122 @@ firebase.initializeApp(config);
 
 const firebaseDB = firebase.database();
 
-firebaseDB.ref('green').set('brown')
-    .then( () => {console.log('data saved')})
-    .catch((e) => {console.log(e);});
+//set update delete
+// firebaseDB.ref('eyes').set(null)
+/*
+firebaseDB.ref().update({
+    name: "steve",
+    'car/color': 'white'
+})
+.then( () => {console.log('data saved')})
+.catch((e) => {console.log(e);});
+*/
+
+// firebaseDB.ref('eyes').set(null) -> set(null) will delete 'eyes' from firebase
 
 // firebase.database().ref().set('it works');
+
+//get data
+/*
+firebaseDB.ref('car/brand').once('value')
+    .then((snapshot) => {
+        console.log(snapshot.val());
+    })
+    .catch((e) => {
+        console.log(e);
+    });
+*/
+//updating data on change in firebase
+/*
+firebaseDB.ref().on('value', (snapshot) => {
+    console.log(snapshot.val());
+});
+*/
+
+/*
+setTimeout(()=> {
+    firebaseDB.ref('name').set('name before off');
+}, 2000);
+
+setTimeout(()=> {
+    firebaseDB.ref().off();
+}, 4000);
+
+setTimeout(()=> {
+    firebaseDB.ref('name').set('name after off');
+}, 7000);
+*/
+
+// firebaseDB.ref().on('child_removed', (snapshot) => {
+// firebaseDB.ref().on('child_changed', (snapshot) => {
+/*
+firebaseDB.ref().on('child_added', (snapshot) => {
+    console.log(snapshot.key, snapshot.val());
+});
+*/
+
+//firebase array
+/*
+firebaseDB.ref('users').push({
+    name: 'randy',
+    lastname: 'Martial'
+});
+*/
+/*
+firebaseDB.ref('users/-LIr0PuBNIfPdaEcUPxy').once('value')
+    .then((snapshot) => {
+        console.log(snapshot.val());
+    });
+*/
+/*
+firebaseDB.ref('users/-LIr0PuBNIfPdaEcUPxy').update({
+   lastname: "updated name"
+});
+*/
+/*
+firebaseDB.ref('users').once('value').then((snapshot) => {
+    const users = [];
+
+    snapshot.forEach((childSnapshot) => {
+        users.push({
+            id: childSnapshot.key,
+            ...childSnapshot.val()
+        });
+    });
+
+    console.log(users);
+});
+*/
+/*
+firebaseDB.ref('users').orderByChild('age').once('value').then((snapshot) => {
+    const users = [];
+
+    snapshot.forEach((childSnapshot) => {
+        users.push({
+            id: childSnapshot.key,
+            ...childSnapshot.val()
+        });
+    });
+
+    console.log(users);
+});
+*/
+/*
+firebaseDB.ref('users').limitToFirst(2).once('value').then((snapshot) => { //limitToLast
+    const users = [];
+
+    snapshot.forEach((childSnapshot) => {
+        users.push({
+            id: childSnapshot.key,
+            ...childSnapshot.val()
+        });
+    });
+
+    console.log(users);
+});
+*/
+/*
+firebaseDB.ref('users').orderByChild('lastname').equalTo('Ball').once('value').then((snapshot) => { //limitToLast
+    console.log(snapshot.val());
+});
+*/
