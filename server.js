51008b0111c6b52222c2cf8440a3e2d504c0e87c
PopovDmytro <dimahom@ukr.net>
2018-10-14 20:10:38 +0300

    insert multiple data and get data from db

--- server.js
+++ server.js
@@ -10,20 +10,40 @@ const url = 'mongodb://localhost:27017/test';
 //     db.close();
 // });
 
-MongoClient.connect(url, (err, client) => {
+// MongoClient.connect(url, (err, client) => {
 
+//     const db = client.db('test');
+
+//     const cars = [
+//         {model: "Fiat", year: 1999},
+//         {model: "Lancia", year: 1986},
+//         {model: "Nissan", year: 2000}
+//     ];
+
+//     // db.collection('Cars').insert(cars, (err,res) => { // insert can be with single and multiple 
+//     db.collection('Cars').insertMany(cars, (err,res) => {
+//         if(err) {
+//             return console.log(`Cannot insert: ${err}`);
+//         }
+
+//         // console.log(res.ops[0]._id.getTimestamp());
+//     });
+//     client.close();
+// });
+
+MongoClient.connect(url, (err, client) => {
     const db = client.db('test');
 
-    db.collection('Cars').insertOne({
-        _od: 777,
-        mode: "Forr",
-        year: 2017
-    }, (err,res) => {
-        if(err) {
-            return console.log(`Cannot insert: ${err}`);
-        }
+    // db.collection('Cars').find({year: 2000})  //find() get all data from collection 'Cars'
+    // .skip(1) //skip element
+    // .limit(1) //limit get elements
+    // .sort({"_id": -1}) //sort get elements //-1 get elements form the end
+    // .toArray()
+    // .then(data => { //promise
+        // console.log(data);
+    // });
 
-        // console.log(res.ops[0]._id.getTimestamp());
-    });
+    db.collection('Cars').findOne({year: 2000}, {model: 0, year: 0}, (err, doc) => {console.log(doc)})
+
     client.close();
 });
