d1f14fdc1705e8b38d5393e66f9911bda5e48480
PopovDmytro <dimahom@ukr.net>
2018-10-14 20:34:24 +0300

    update and delete mongoDB

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const url = 'mongodb://localhost:27017/test';
 //     db.close();
 // });
 
+/* insert data to DB */
+
 // MongoClient.connect(url, (err, client) => {
 
 //     const db = client.db('test');
@@ -31,8 +33,10 @@ const url = 'mongodb://localhost:27017/test';
 //     client.close();
 // });
 
-MongoClient.connect(url, (err, client) => {
-    const db = client.db('test');
+/* get data from DB */
+
+// MongoClient.connect(url, (err, client) => {
+    // const db = client.db('test');
 
     // db.collection('Cars').find({year: 2000})  //find() get all data from collection 'Cars'
     // .skip(1) //skip element
@@ -43,7 +47,46 @@ MongoClient.connect(url, (err, client) => {
         // console.log(data);
     // });
 
-    db.collection('Cars').findOne({year: 2000}, {model: 0, year: 0}, (err, doc) => {console.log(doc)})
+    // db.collection('Cars').findOne({year: 2000}, {model: 0, year: 0}, (err, doc) => {console.log(doc)})
+
+    // client.close();
+// });
+
+/* delete data in DB */
+
+// MongoClient.connect(url, (err, client) => {
+    // const db = client.db('test');
+
+    // db.collection('Cars').deleteMany({year: 2000}, (err, doc) => {
+    // db.collection('Cars').deleteOne({year: 2000}, (err, doc) => {
+    // db.collection('Cars').findOneAndDelete({year: 2000}, (err, doc) => {
+    //     console.log(doc);
+    // });
+
+    // db.collection('Cars').findOneAndDelete({year: 2000}, (err, doc) => {
+        // console.log(doc);
+    // });
+
+    // client.close();
+// });
+
+/* update data in DB */
+
+MongoClient.connect(url, (err, client) => {
+    const db = client.db('test');
+
+    db.collection('Users').findOneAndUpdate(
+        {name : "Greg"},
+        {
+            $set : {lastname: "Steevenson"},
+            $inc: {age: +2}
+        },
+        {
+            retutnOriginal: true,
+            upsert: true // will create new element in db if not exist
+        },
+        (err, doc) => {console.log(doc)}
+    );
 
     client.close();
 });
