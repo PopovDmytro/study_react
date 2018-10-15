47c3b45d8e96ebc8e62e57e94ad1e03cc45836cf
PopovDmytro <dimahom@ukr.net>
2018-10-15 11:27:02 +0300

    mongoose insert, get, delete, update data

--- server.js
+++ server.js
@@ -1,92 +1,84 @@
-const {MongoClient} = require('mongodb');
+const mongoose = require('mongoose');
 
 const url = 'mongodb://localhost:27017/test';
 
-// MongoClient.connect(url, (err, db) => {
-//     if(err) {
-//         console.log('could not connect');
-//     }
-//     console.log('connected !!!');
-//     db.close();
-// });
+mongoose.Promise = global.Promise;
+mongoose.connect(url);
 
-/* insert data to DB */
-
-// MongoClient.connect(url, (err, client) => {
-
-//     const db = client.db('test');
-
-//     const cars = [
-//         {model: "Fiat", year: 1999},
-//         {model: "Lancia", year: 1986},
-//         {model: "Nissan", year: 2000}
-//     ];
-
-//     // db.collection('Cars').insert(cars, (err,res) => { // insert can be with single and multiple 
-//     db.collection('Cars').insertMany(cars, (err,res) => {
-//         if(err) {
-//             return console.log(`Cannot insert: ${err}`);
-//         }
-
-//         // console.log(res.ops[0]._id.getTimestamp());
-//     });
-//     client.close();
-// });
-
-/* get data from DB */
-
-// MongoClient.connect(url, (err, client) => {
-    // const db = client.db('test');
-
-    // db.collection('Cars').find({year: 2000})  //find() get all data from collection 'Cars'
-    // .skip(1) //skip element
-    // .limit(1) //limit get elements
-    // .sort({"_id": -1}) //sort get elements //-1 get elements form the end
-    // .toArray()
-    // .then(data => { //promise
-        // console.log(data);
-    // });
-
-    // db.collection('Cars').findOne({year: 2000}, {model: 0, year: 0}, (err, doc) => {console.log(doc)})
-
-    // client.close();
-// });
-
-/* delete data in DB */
-
-// MongoClient.connect(url, (err, client) => {
-    // const db = client.db('test');
-
-    // db.collection('Cars').deleteMany({year: 2000}, (err, doc) => {
-    // db.collection('Cars').deleteOne({year: 2000}, (err, doc) => {
-    // db.collection('Cars').findOneAndDelete({year: 2000}, (err, doc) => {
-    //     console.log(doc);
-    // });
-
-    // db.collection('Cars').findOneAndDelete({year: 2000}, (err, doc) => {
-        // console.log(doc);
-    // });
-
-    // client.close();
-// });
-
-/* update data in DB */
-
-MongoClient.connect(url, (err, client) => {
-    const db = client.db('test');
-
-    db.collection('Users').findOneAndUpdate(
-        {name : "Greg"},
-        {
-            $set : {lastname: "Steevenson"},
-            $inc: {age: +2}
-        },
-        {
-            retutnOriginal: true,
-            upsert: true // will create new element in db if not exist
-        },
-        (err, doc) => {console.log(doc)}
-    );
-
-    client.close();
+const carSchema = mongoose.Schema({
+    brand: String,
+    model: String,
+    year: Number,
+    avail: Boolean
 });
+
+const Car = mongoose.model('Car', carSchema);
+
+// const addCar = new Car({
+//     brand: "Mersedes",
+//     model: "cl",
+//     year: 2015,
+//     avail: true
+// });
+
+/* insert */
+
+// addCar.save((err, doc) => {
+//     if(err) return console.log(err);
+//     console.log(doc);
+// });
+
+/* get */
+
+// Car.find({_id: "5bc43fd620debd32b023bfa2"}, (err, doc) => {
+// Car.findById("5bc43fd620debd32b023bfa2", (err, doc) => {
+//     if(err) return console.log(err);
+
+//     console.log(doc);
+// });
+
+/* delete */
+
+// Car.findOneAndRemove({brand: "Ford"}, (err, doc) => {
+// Car.findByIdAndRemove("5bc43fd620debd32b023bfa2", (err, doc) => {
+// Car.remove({}, (err, doc) => {                                           // wipe database
+// Car.remove({year: 2000}, (err, doc) => {
+//     if(err) return console.log(err);
+
+//     console.log(doc);
+// });
+
+/* upgrade */
+
+// Car.update(
+//     {_id: "5bc43fd620debd32b023bfa2"},
+//     {$set: {brand: "Traban"}},
+//     (err, doc) => {
+//     if(err) return console.log(err);
+//     console.log(doc);
+// });
+
+// Car.findByIdAndUpdate(
+//     "5bc43fd620debd32b023bfa2",
+//     {$set: {brand: "Skoda"}},
+//     (err, doc) => {
+//         if(err) return console.log(err);
+//         console.log(doc);
+//     }
+// );
+
+/**
+ * 
+ */
+Car.findById("5bc43fd620debd32b023bfa2", (err, car) => {
+    if(err) return console.log(err);
+
+    car.set({
+        brand: "Porche"
+    });
+
+    car.save((err, doc) => {
+        if(err) return console.log(err);
+        console.log(doc);
+    });
+})
